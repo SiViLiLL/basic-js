6896c38d6b93d2db97101c22de44439f0f8f0ff4
Dilyara Arslanova <risharsivil@gmail.com>
2023-04-17 20:29:07 +0300

    refactor: getCommonCharacterCount func

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,33 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-	let count = 0;
-	let obj1 = {};
-	let obj2 = {};
-	for (let i = 0; i < s1.length; i++){
-		 if (obj1[s1[i]] === undefined){
-			  obj1[s1[i]] = 1;
-		 } else obj1[s1[i]] += 1
-	}
-	for (let i = 0; i < s2.length; i++){
-		 if (obj2[s2[i]] === undefined){
-			  obj2[s2[i]] = 1;
-		 } else obj2[s2[i]] += 1
-	}
-	if (s1.length >= s2.length){
-		 for (let property in obj2){
-			  if (obj1[property] !== undefined){
-					obj1[property] < obj2[property]? count += obj1[property]: count += obj2[property]
-			  }
-		 }
-	} else {
-		 for (let property in obj1){
-			  if (obj2[property] !== undefined){
-					obj1[property] < obj2[property]? count += obj1[property]: count += obj2[property]
-			  }
-		 }
-	}
-	return count
+  const commLetters = s1.split('')
+    .reduce((acc, letter) => {
+      if (s2.indexOf(letter) !== -1) {
+        acc[letter] = acc[letter] ? acc[letter] + 1 : 1;
+      }
+      return acc
+    }, {});
+  return s2.split('').reduce((acc, letter) => {
+      if (commLetters[letter]) {
+          commLetters[letter] -= 1;
+          return acc + 1;
+      }
+      return acc;
+  }, 0)
 }
 
 module.exports = {
